985744db56ea96ac4c1d673f6e5d86371832629e
Marina Vystavkina <vistavkinamm@mail.ru>
2022-08-23 16:38:21 +0300

    Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
